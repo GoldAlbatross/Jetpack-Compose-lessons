07bad2c5416f88ebe72302aa1d96369a585fd117
Petr Gnesny <snovaodin@gmail.com>
2023-11-14 16:13:05 +0300

    6. передача данных в функцию: mutableStateOf

--- app/src/main/java/com/lessons/jetpackcompose/HomeScreen.kt
+++ app/src/main/java/com/lessons/jetpackcompose/HomeScreen.kt
@@ -1,68 +1,29 @@
 package com.lessons.jetpackcompose
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.BottomCenter
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
-import androidx.compose.ui.Alignment.Companion.Start
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.LinearGradient
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import java.nio.file.WatchEvent
 
 @Composable
-fun HomeScreen() {
-    val gradient = Brush.linearGradient(colors = listOf(Color.Red,Color.Yellow, Color.Green))
-    Box(
-        contentAlignment = Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        AsyncImage(
-            model = "https://static.tildacdn.com/tild6531-6334-4461-b235-643034643533/Jennifer-Lopez-2019-.jpg",
-            contentDescription = null,
-            modifier = Modifier
-                .width(260.dp)
-                .height(260.dp)
-                .clip(CircleShape)
-                .border(4.dp, gradient, CircleShape)
-                .border(10.dp, Color.White, CircleShape),
-            contentScale = ContentScale.Crop
-        )
-    }
+fun HomeScreen(
+    count: State<Int>,
+    counter: () -> Unit,
+) {
+    val counterValue = count.value
+    Text(
+        text = "Clicks: $counterValue",
+        modifier = Modifier.clickable(onClick = { counter() })
+    )
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview() {
-    HomeScreen()
+//@Preview(showBackground = true)
+//@Composable
+//fun HomeScreenPreview() {
+//    HomeScreen()
+//}
+class Counter {
+    var value = 0
 }
--- app/src/main/java/com/lessons/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/lessons/jetpackcompose/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lessons.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -15,8 +16,10 @@ import com.lessons.jetpackcompose.ui.theme.JetpackComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val count = mutableStateOf(0)
         setContent {
-            HomeScreen()
+            HomeScreen(count = count, counter = { count.value++ })
         }
     }
 }
